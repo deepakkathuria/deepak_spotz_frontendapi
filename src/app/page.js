de4b37acff4d18f013a6600540f7b9f3f2036738
Hi-Gaurav <hi.kshatriya@gmail.com>
2023-06-09 12:17:03 +0530

    gaurav

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./page.module.css";
 import NewsSection from "@/components/common/NewsSection";
+import NewsSectionLatest from "../components/common/NewsSectionLatest";
 import axios from "axios";
 import MobSecondaryNav from "@/components/common/MobSecondaryNav";
 const base_url = process.env.NEXT_PUBLIC_BASE_URL;
@@ -9,21 +10,44 @@ import ScoreCardPanel from "@/components/common/ScoreCardPanel";
 const site_url = process.env.NEXT_PUBLIC_SITE_URL;
 
 const page = async () => {
-  var categoriesList = await axios
-    .get(`${base_url}/getrecentcategorynameslug`)
-    .catch((err) => console.log(err));
+  try {
+    const response = await fetch(`${base_url}/getrecentcategorynameslug`, {
+      next: { revalidate: 60 },
+    });
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    var categoriesList = await response.json();
+    // Use the categoriesList data here
+  } catch (err) {
+    console.log(err);
+  }
 
   var distinctCategories = Array.from(
-    new Set(categoriesList?.data.map((item) => item.category_slug))
+    new Set(categoriesList?.map((item) => item.category_slug))
   );
 
   try {
-    var data = await axios.get(
-      `${base_url}/getcategoriesbyname?category1=${distinctCategories[0]}&category2=${distinctCategories[1]}&category3=${distinctCategories[2]}`
+    const response = await fetch(
+      `${base_url}/getcategoriesbyname?category1=${distinctCategories[0]}&category2=${distinctCategories[1]}&category3=${distinctCategories[2]}`,
+      { next: { revalidate: 60 } }
     );
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    var data = await response.json();
+    // Use the data here
   } catch (e) {
     console.log(e.message);
   }
+
+  // try {
+  //   var latestData = await axios.get(
+  //     `${base_url}/getlatestposts?limit=9&page=1`
+  //   );
+  // } catch (e) {
+  //   console.log(e.message);
+  // }
 
   return (
     <>
@@ -69,20 +93,12 @@ const page = async () => {
       />
       <MobSecondaryNav />
       <div className={styles.homeContainer}>
-        {/* <ScoreCardPanel /> */}
+        <ScoreCardPanel />
 
-        {/* <div className={styles.newsSectionDiv}>
-          {data?.data?.map((item) => {
-            return (
-              <div key={item.ID}>
-                <NewsSection name={item.name} slug={item.slug} />
-              </div>
-            );
-          })}
-        </div> */}
+        <NewsSectionLatest />
 
         <div className={styles.newsSectionDiv}>
-          {data?.data?.map((item) => {
+          {data?.map((item) => {
             return (
               <div key={item.ID}>
                 <NewsSection name={item.name} slug={item.slug} />
---
+++ src/components/common/NewsSectionLatest.js
@@ -0,0 +1,53 @@
+import React from "react";
+import styles from "../styles/NewsSection.module.css";
+// import { GiCricketBat } from "react-icons/gi";
+import ButtonTab from "./ButtonTab";
+import NewsCard from "./NewsCard";
+import Link from "next/link";
+const base_url = process.env.NEXT_PUBLIC_BASE_URL;
+
+const NewsSection = async (props) => {
+  try {
+    var response = await fetch(`${base_url}/getlatestposts?limit=9&page=1`, {
+      next: { revalidate: 1 },
+    });
+    if (response.ok) {
+      var data = await response.json();
+    } else {
+      console.log("Error: " + response.status);
+    }
+  } catch (e) {
+    console.log(e.message);
+  }
+
+  return (
+    <>
+      <div className={styles.newsSectionContainer}>
+        <div className={styles.newsSectionHeading}>
+          <h3>LATEST</h3>
+        </div>
+        <div className={styles.newsCardsDiv}>
+          {data?.map((item) => {
+            return (
+              <div key={item.id}>
+                <Link href={`${props.slug}/${item.post_name}`}>
+                  <NewsCard
+                    title={item.post_title}
+                    date={new Date(item.post_modified_gmt).toLocaleString()}
+                    content={item.post_content}
+                    // slug={item.name}
+                  />
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+        <div className={styles.readMoreLabel}>
+          <Link href={`/${props.slug}`}>Read More</Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NewsSection;

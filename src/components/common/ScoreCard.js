b92eafaf8d2882f52541f8a94700668d57d44c77
Hi-Gaurav <hi.kshatriya@gmail.com>
2023-06-19 19:58:37 +0530

    hydration fix try 1

--- src/components/common/ScoreCard.js
+++ src/components/common/ScoreCard.js
@@ -1,3 +1,4 @@
+"use client"
 import React from "react";
 import styles from "../styles/ScoreCard.module.css";
 import Image from "next/image";
--- src/components/common/UpdatesSound.js
+++ src/components/common/UpdatesSound.js
@@ -1,4 +1,4 @@
-// "use client";
+"use client";
 import React from "react";
 import styles from "../styles/UpdateSound.module.css";
 import Image from "next/image";

20c3a54875a29982b801d980564323321b2adcc0
Hi-Gaurav <hi.kshatriya@gmail.com>
2023-08-10 22:39:57 +0530

    removed more testing files

--- src/app/testing/series/venues/page.js
+++ src/app/testing/series/venues/page.js
@@ -1,43 +1,53 @@
-import React from "react";
-import styles from "./seriesVenue.module.css";
-import UpdatesSound from "@/components/common/UpdatesSound";
-import VenueCard from "@/components/common/VenueCard";
+import React from 'react'
 
 const page = () => {
   return (
-    <>
-      <div className={styles.container}>
-        <div className="updateBox">
-          <UpdatesSound />
-        </div>
-        <div className={styles.seriesOverviewTitle}>
-          <p>Sri Lanka Tour of india 2022</p>
-        </div>
-        <div className={styles.tertiaryNav}>
-          <ul className={styles.tertiaryNavUl}>
-            <li className={`${styles.tertiaryNavLi} ${styles.active}`}>
-              Overview
-            </li>
-            <li className={styles.tertiaryNavLi}>Schedule & Results</li>
-            <li className={styles.tertiaryNavLi}>News</li>
-            <li className={styles.tertiaryNavLi}>Stats</li>
-            <li className={styles.tertiaryNavLi}>Squads</li>
-            <li className={styles.tertiaryNavLi}>Venues</li>
-            <li className={styles.tertiaryNavLi}>Videos</li>
-            <li className={styles.tertiaryNavLi}>Photos</li>
-          </ul>
-        </div>
-        <div className={styles.venues}>
-          <VenueCard />
-          <VenueCard />
-          <VenueCard />
-          <VenueCard />
-          <VenueCard />
-          <VenueCard />
-        </div>
-      </div>
-    </>
-  );
-};
+    <div>page</div>
+  )
+}
 
-export default page;
+export default page
+
+// import React from "react";
+// import styles from "./seriesVenue.module.css";
+// import UpdatesSound from "@/components/common/UpdatesSound";
+// import VenueCard from "@/components/common/VenueCard";
+
+// const page = () => {
+//   return (
+//     <>
+//       <div className={styles.container}>
+//         <div className="updateBox">
+//           <UpdatesSound />
+//         </div>
+//         <div className={styles.seriesOverviewTitle}>
+//           <p>Sri Lanka Tour of india 2022</p>
+//         </div>
+//         <div className={styles.tertiaryNav}>
+//           <ul className={styles.tertiaryNavUl}>
+//             <li className={`${styles.tertiaryNavLi} ${styles.active}`}>
+//               Overview
+//             </li>
+//             <li className={styles.tertiaryNavLi}>Schedule & Results</li>
+//             <li className={styles.tertiaryNavLi}>News</li>
+//             <li className={styles.tertiaryNavLi}>Stats</li>
+//             <li className={styles.tertiaryNavLi}>Squads</li>
+//             <li className={styles.tertiaryNavLi}>Venues</li>
+//             <li className={styles.tertiaryNavLi}>Videos</li>
+//             <li className={styles.tertiaryNavLi}>Photos</li>
+//           </ul>
+//         </div>
+//         <div className={styles.venues}>
+//           <VenueCard />
+//           <VenueCard />
+//           <VenueCard />
+//           <VenueCard />
+//           <VenueCard />
+//           <VenueCard />
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default page;

3f55312e5daff26d3413dcaecfc61f8d2e6ace9c
Hi-Gaurav <hi.kshatriya@gmail.com>
2023-08-18 13:05:05 +0530

    navsecscore css

--- src/components/liveScore/NavSecScore.js
+++ src/components/liveScore/NavSecScore.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./NavSec.module.css";
+import styles from "./NavSecScore.module.css";
 import Link from "next/link";
 
 const NavSecScore = (props) => {

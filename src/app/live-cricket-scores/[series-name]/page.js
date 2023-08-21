43406fa7d588930c37a41ebb59bf573d8ffc1d7a
Hi-Gaurav <hi.kshatriya@gmail.com>
2023-08-21 13:45:38 +0530

    use client added

--- src/app/live-cricket-scores/[series-name]/page.js
+++ src/app/live-cricket-scores/[series-name]/page.js
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState, useEffect } from "react";
 import ScorePanel from "../../../components/scorePage/ScorePanel";
 import NavBarSec from "../../../components/scorePage/NavBarSec";
